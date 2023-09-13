d79ee479a2144e941d8927b1ca87d70e692aa613
brophie2 <brophie21119@gmail.com>
2023-09-13 19:35:46 +1000

    commit 2

---
+++ _posts/ramen-eggs.md
@@ -0,0 +1,27 @@
+---
+title: 'Ramen Eggs'
+excerpt: 'This is perfect to meal prep. Keeping it in the fridge for a couple of days actually helps the yolk have a more jammy texture.'
+coverImage: '/assets/recipe/ramen-eggs/cover.jpg'
+date: '2020-03-16T05:35:07.322Z'
+ogImage:
+  url: '/assets/recipe/ramen-eggs/cover.jpg'
+---
+
+This is perfect to meal prep. Keeping it in the fridge for a couple of days actually helps the yolk have a more jammy texture. 
+
+The reason we have diluted the marinade is so that it can sit in the marinade for longer without becoming overly salty. We also found that letting the eggs just sit in the marinade for a shorter period of time, to reduce the saltiness, wasn't ideal as we ended up requiring a lot more marinade which often ended up being wasted. Instead, we use the leftover marinade on our eggs, as a soup base or as a sauce with our rice/noodles.
+
+There are lots of optional extras you can add to the marinade, such as chili, garlic or spring onions. This time, we kept it simple by just adding spring onions. 
+
+Feel free to soft or hard boil your eggs to your liking. We chose to bring the water to a boil and then lower in the eggs for 7 minutes. Make sure to put lots of salt or vinegar in the water and have an ice bath ready for the eggs so that it makes it easier to peel. 
+
+Ingredients: 
+- `1/4 cup` soy sauce
+- `1/8 cup` mirin
+- `3/4 cup` water
+- Sesame seeds
+- Cooked eggs
+
+Method: 
+1. Combine the soy sauce, mirin, water and sesame seeds in a bowl or container. Make sure the bowl/container is large enough to accommodate the eggs as well
+2. Gently lower the eggs into the marinate and refrigerate overnight
---
+++ pages/recipes/[slug].tsx
@@ -0,0 +1,94 @@
+import ErrorPage from 'next/error'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import Container from '../../components/container'
+import Header from '../../components/header'
+import Layout from '../../components/layout'
+import RecipeBody from '../../components/recipe-body'
+import RecipeHeader from '../../components/recipe-header'
+import RecipeTitle from '../../components/recipe-title'
+import type PostType from '../../interfaces/post'
+import { getAllPosts, getPostBySlug } from '../../lib/api'
+import { CMS_NAME } from '../../lib/constants'
+import markdownToHtml from '../../lib/markdownToHtml'
+
+type Props = {
+  post: PostType
+  morePosts: PostType[]
+  preview?: boolean
+}
+
+export default function Post({ post, morePosts, preview }: Props) {
+  const router = useRouter()
+  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />
+  }
+  return (
+    <Layout preview={preview}>
+      <Container>
+        <Header />
+        {router.isFallback ? (
+          <RecipeTitle>Loading…</RecipeTitle>
+        ) : (
+          <>
+            <article className="mb-32">
+              <Head>
+                <title>{title}</title>
+                <meta property="og:image" content={post.ogImage.url} />
+              </Head>
+              <RecipeHeader
+                title={post.title}
+                coverImage={post.coverImage}
+                date={post.date}
+              />
+              <RecipeBody content={post.content} />
+            </article>
+          </>
+        )}
+      </Container>
+    </Layout>
+  )
+}
+
+type Params = {
+  params: {
+    slug: string
+  }
+}
+
+export async function getStaticProps({ params }: Params) {
+  const post = getPostBySlug(params.slug, [
+    'title',
+    'date',
+    'slug',
+    'content',
+    'ogImage',
+    'coverImage',
+  ])
+  const content = await markdownToHtml(post.content || '')
+
+  return {
+    props: {
+      post: {
+        ...post,
+        content,
+      },
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts(['slug'])
+
+  return {
+    paths: posts.map((post) => {
+      return {
+        params: {
+          slug: post.slug,
+        },
+      }
+    }),
+    fallback: false,
+  }
+}
