3459b90891ddb57d3f282aaa825e4eeb768a6ffa
Sophie Lee-Goh <sophieleegoh@gmail.com>
2024-02-18 20:37:29 +1100

    Add filter to recipe index

--- components/common/dropdown.tsx
+++ components/common/dropdown.tsx
@@ -1,24 +1,32 @@
-import { Fragment } from 'react'
-import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
-import React from 'react';
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import React from "react";
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 type Props = {
   title: string;
-  dropdownOptions: string[];
+  dropdownOptions: DropdownOption[];
 };
 
-export default function Dropdown({title, dropdownOptions}: Props) {
+type DropdownOption = {
+  name: string;
+  action: () => void;
+};
+
+export default function Dropdown({ title, dropdownOptions }: Props) {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           {title}
-        <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+          <ChevronDownIcon
+            className="-mr-1 h-5 w-5 text-gray-400"
+            aria-hidden="true"
+          />
         </Menu.Button>
       </div>
 
@@ -31,30 +39,32 @@ export default function Dropdown({title, dropdownOptions}: Props) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute left-0 z-10 mt-2 w-60 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             {dropdownOptions.map((opt) => {
               return (
-                <React.Fragment key={opt}>
+                <React.Fragment key={opt.name}>
                   <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href=""
-                      className={classNames(
-                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                        'block px-4 py-2 text-sm'
-                      )}
-                    >
-                      {opt}
-                    </a>
-                  )}
-                </Menu.Item> 
-                </React.Fragment> );
+                    {({ active }) => (
+                      <button
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-xs w-full text-left"
+                        )}
+                        onClick={opt.action}
+                      >
+                        {opt.name}
+                      </button>
+                    )}
+                  </Menu.Item>
+                </React.Fragment>
+              );
             })}
-
           </div>
         </Menu.Items>
       </Transition>
     </Menu>
-  )
+  );
 }
--- pages/allrecipes/index.tsx
+++ pages/allrecipes/index.tsx
@@ -1,4 +1,5 @@
 import Container from "@/components/common/container";
+import Dropdown from "@/components/common/dropdown";
 import Header from "@/components/common/header";
 import Layout from "@/components/common/layout";
 import RecipeListItem from "@/components/recipe/recipe-list-item";
@@ -6,7 +7,7 @@ import RecipeTitle from "@/components/recipe/recipe-title";
 import Recipe from "@/interfaces/recipes";
 import { getAllRecipes } from "@/lib/api";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {
   allRecipes: Recipe[];
@@ -14,6 +15,22 @@ type Props = {
 
 export default function AllRecipes({ allRecipes }: Props) {
   const router = useRouter();
+  const [ sortedRecipes, setSortedRecipes ] = useState(allRecipes);
+  const [ filterTitle, setFilterTitle ] = useState('Filter');
+
+  const showRecipes = (
+    <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 mb-16">
+      {allRecipes.map((recipe) => (
+        <RecipeListItem
+          key={recipe.title}
+          title={recipe.title}
+          slug={recipe.slug}
+          coverImage={recipe.coverImage}
+        />
+      ))}
+    </div>
+  );
+
   return (
     <>
       <Header />
@@ -22,16 +39,65 @@ export default function AllRecipes({ allRecipes }: Props) {
           {router.isFallback ? (
             <RecipeTitle>Loading…</RecipeTitle>
           ) : (
-            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 mb-16">
-              {allRecipes.map((recipe) => (
-                <RecipeListItem
-                  key={recipe.title}
-                  title={recipe.title}
-                  slug={recipe.slug}
-                  coverImage={recipe.coverImage}
+            <>
+              <h1 className="mb-16 text-4xl font-bold tracking-tighter leading-tight">
+                All Recipes
+              </h1>
+              <div className="pb-8">
+                <Dropdown
+                  title={filterTitle}
+                  dropdownOptions={[
+                    {
+                      name: "Name (A-Z)",
+                      action: () => {
+                          setSortedRecipes([
+                          ...allRecipes.sort((recipe1, recipe2) =>
+                            recipe1.title < recipe2.title ? -1 : 1
+                          ),
+                        ]);
+                        setFilterTitle("Name (A-Z)")
+                        },
+                      
+                    },
+                    {
+                      name: "Name (Z-A)",
+                      action: () => {
+                          setSortedRecipes([
+                            ...allRecipes.sort((recipe1, recipe2) =>
+                              recipe1.title > recipe2.title ? -1 : 1
+                            ),
+                          ]);
+                          setFilterTitle("Name (Z-A)")
+                        }
+                    },
+                    {
+                      name: "Date posted (Latest first)",
+                      action: () => {
+                        setSortedRecipes([
+                          ...allRecipes.sort((recipe1, recipe2) =>
+                            recipe1.date > recipe2.date ? -1 : 1
+                          ),
+                        ])
+                        setFilterTitle("Date posted (Latest first)")
+                      }
+                    },
+                    {
+                      name: "Date posted (Earliest first)",
+                      action: () => {
+                        setSortedRecipes([
+                          ...allRecipes.sort((recipe1, recipe2) =>
+                            recipe1.date < recipe2.date ? -1 : 1
+                          ),
+                        ])
+                        setFilterTitle("Date posted (Earliest first)")
+                      }
+                    },
+                  ]}
                 />
-              ))}
-            </div>
+              </div>
+
+              {showRecipes}
+            </>
           )}
         </Container>
       </Layout>
